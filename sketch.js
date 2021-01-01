6f2255f5b99f40537963f14ef7af026e62376251
shashankvini <72648660+shashankvini@users.noreply.github.com>
2021-01-01 18:32:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+recipie for noodle omlete
+step 1- make maggie of 2 packets 
+step 2- keep the maggie closed until the water evaporates
+step 3- transfer the maggie into a bowl 
+step 4- add 3 eggs to it and add salt and chilli powder 
+step 5- mix the items 
+step 6- take a frying pan and add oil to it
+step 7- add the mixture in smaller quantities once at a Time
+step 8- close the pan with a lid after adding the mixture
+step 9- wait for 2 min and flip the omlete and fry it for 2 min and Then
+step 10 - tasty omlete with noodles is ready to Eat
